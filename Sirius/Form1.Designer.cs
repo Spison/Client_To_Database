a4ce8c94d596ce23b6edd3d7fca8e9877dc76cd4
Spison <sanya.kanash@yandex.ru>
2022-12-17 20:53:08 +0300

    Added query of statistic

--- Sirius/Form1.Designer.cs
+++ Sirius/Form1.Designer.cs
@@ -28,11 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonRefresh = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.buttonSearch = new System.Windows.Forms.Button();
+            this.listBox_Status = new System.Windows.Forms.ListBox();
+            this.statusBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.siriusDataSet = new Sirius.SiriusDataSet();
+            this.listBox_EmployOrUnemploy = new System.Windows.Forms.ListBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button_StatisticOfEmploy = new System.Windows.Forms.Button();
+            this.statusTableAdapter = new Sirius.SiriusDataSetTableAdapters.statusTableAdapter();
+            this.labelStatus = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.statusBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.siriusDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -55,7 +67,7 @@
             this.buttonRefresh.Size = new System.Drawing.Size(40, 40);
             this.buttonRefresh.TabIndex = 1;
             this.buttonRefresh.UseVisualStyleBackColor = true;
-            this.buttonRefresh.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonRefresh_MouseClick);
+            this.buttonRefresh.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonRefresh_MouseClick);
             // 
             // textBox1
             // 
@@ -73,13 +85,91 @@
             this.buttonSearch.Size = new System.Drawing.Size(40, 40);
             this.buttonSearch.TabIndex = 3;
             this.buttonSearch.UseVisualStyleBackColor = true;
-            this.buttonSearch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonFind_MouseClick);
+            this.buttonSearch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonFind_MouseClick);
+            // 
+            // listBox_Status
+            // 
+            this.listBox_Status.DataSource = this.statusBindingSource;
+            this.listBox_Status.DisplayMember = "name";
+            this.listBox_Status.FormattingEnabled = true;
+            this.listBox_Status.Location = new System.Drawing.Point(37, 256);
+            this.listBox_Status.Name = "listBox_Status";
+            this.listBox_Status.Size = new System.Drawing.Size(120, 30);
+            this.listBox_Status.TabIndex = 4;
+            this.listBox_Status.ValueMember = "name";
+            this.listBox_Status.SelectedIndexChanged += new System.EventHandler(this.ListBox_Status_SelectedIndexChanged);
+            // 
+            // statusBindingSource
+            // 
+            this.statusBindingSource.DataMember = "status";
+            this.statusBindingSource.DataSource = this.siriusDataSet;
+            // 
+            // siriusDataSet
+            // 
+            this.siriusDataSet.DataSetName = "SiriusDataSet";
+            this.siriusDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // listBox_EmployOrUnemploy
+            // 
+            this.listBox_EmployOrUnemploy.FormattingEnabled = true;
+            this.listBox_EmployOrUnemploy.Items.AddRange(new object[] {
+            "(нет)",
+            "Принятые",
+            "Уволенные"});
+            this.listBox_EmployOrUnemploy.Location = new System.Drawing.Point(37, 292);
+            this.listBox_EmployOrUnemploy.Name = "listBox_EmployOrUnemploy";
+            this.listBox_EmployOrUnemploy.Size = new System.Drawing.Size(120, 43);
+            this.listBox_EmployOrUnemploy.TabIndex = 5;
+            this.listBox_EmployOrUnemploy.SelectedIndexChanged += new System.EventHandler(this.ListBox_EmployOrUnemploy_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(183, 289);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(425, 289);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 7;
+            // 
+            // button_StatisticOfEmploy
+            // 
+            this.button_StatisticOfEmploy.Location = new System.Drawing.Point(342, 315);
+            this.button_StatisticOfEmploy.Name = "button_StatisticOfEmploy";
+            this.button_StatisticOfEmploy.Size = new System.Drawing.Size(127, 23);
+            this.button_StatisticOfEmploy.TabIndex = 8;
+            this.button_StatisticOfEmploy.Text = "Получить статистику";
+            this.button_StatisticOfEmploy.UseVisualStyleBackColor = true;
+            this.button_StatisticOfEmploy.Click += new System.EventHandler(this.Button_StatisticOfEmploy_Click);
+            // 
+            // statusTableAdapter
+            // 
+            this.statusTableAdapter.ClearBeforeFill = true;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(183, 256);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(35, 13);
+            this.labelStatus.TabIndex = 9;
+            this.labelStatus.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.button_StatisticOfEmploy);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.listBox_EmployOrUnemploy);
+            this.Controls.Add(this.listBox_Status);
             this.Controls.Add(this.buttonSearch);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.buttonRefresh);
@@ -88,6 +178,8 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.statusBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.siriusDataSet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,6 +191,15 @@
         private System.Windows.Forms.Button buttonRefresh;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listBox_Status;
+        private System.Windows.Forms.ListBox listBox_EmployOrUnemploy;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button_StatisticOfEmploy;
+        private SiriusDataSet siriusDataSet;
+        private System.Windows.Forms.BindingSource statusBindingSource;
+        private SiriusDataSetTableAdapters.statusTableAdapter statusTableAdapter;
+        private System.Windows.Forms.Label labelStatus;
     }
 }
 
--- Sirius/Form1.cs
+++ Sirius/Form1.cs
@@ -23,7 +23,6 @@ namespace Sirius
     public partial class Form1 : Form
     {
         DataBase database = new DataBase();
-        int selectedRow;
         public Form1()
         {
             InitializeComponent();
@@ -31,6 +30,9 @@ namespace Sirius
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "siriusDataSet.status". При необходимости она может быть перемещена или удалена.
+            this.statusTableAdapter.Fill(this.siriusDataSet.status);
+            listBox_EmployOrUnemploy.SelectedIndex = 0;
             CreateColums();
             RefreshDataGrid(dataGridView1,"");
         }
@@ -75,16 +77,75 @@ namespace Sirius
                 ReadSingleRow(dgw, reader);
             }
             reader.Close();
+            database.closeConnection();
         }
-        private void buttonFind_MouseClick(object sender, MouseEventArgs e)
+        private void ButtonFind_MouseClick(object sender, MouseEventArgs e)
         {
             string line = textBox1.Text;     
             RefreshDataGrid(dataGridView1, line);
         }
 
-        private void buttonRefresh_MouseClick(object sender, MouseEventArgs e)
+        private void ButtonRefresh_MouseClick(object sender, MouseEventArgs e)
         {
+            textBox1.Text = "";
             RefreshDataGrid(dataGridView1, "");
+        }
+
+        private void ListBox_EmployOrUnemploy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox_EmployOrUnemploy.SelectedIndex != 0) { dateTimePicker1.Visible = true; dateTimePicker2.Visible = true; button_StatisticOfEmploy.Enabled = true; button_StatisticOfEmploy.Visible = true; }
+            if (listBox_EmployOrUnemploy.SelectedIndex == 0) { dateTimePicker1.Visible = false; dateTimePicker2.Visible = false; button_StatisticOfEmploy.Enabled = false; button_StatisticOfEmploy.Visible = false; }
+        }
+
+        private void ListBox_Status_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var line = listBox_Status.SelectedValue;
+            
+            labelStatus.Visible = true;
+
+            string queryString;
+            queryString = $"SELECT  COUNT(*) FROM status INNER JOIN persons ON status.id = status WHERE status.name = '{line}'";
+
+            SqlCommand cmd = new SqlCommand(queryString, database.GetConnection());
+            database.openConnection();
+            SqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            labelStatus.Text = "Число работников со статусом "+line+": "+reader.GetInt32(0);
+            
+            database.closeConnection();
+        }
+
+        private void Button_StatisticOfEmploy_Click(object sender, EventArgs e)
+        {
+            var dateStart = dateTimePicker1.Value;
+            var dateEnd = dateTimePicker2.Value;
+            if(dateStart > dateEnd) 
+            {
+                MessageBox.Show("Были выбраны неверные даты");
+                return;
+            }
+            string queryString="";
+            if (listBox_EmployOrUnemploy.SelectedIndex == 1) { queryString = $"SELECT last_name, first_name, second_name, date_employ FROM persons WHERE date_employ >= '{dateStart}' AND date_employ<= '{dateEnd}'"; }
+            else if (listBox_EmployOrUnemploy.SelectedIndex == 2) { queryString = $"SELECT last_name, first_name, second_name, date_uneploy FROM persons WHERE date_uneploy >= '{dateStart}' AND date_uneploy<= '{dateEnd}'"; }
+            else return;
+
+            SqlCommand cmd = new SqlCommand(queryString, database.GetConnection());
+            database.openConnection();
+            SqlDataReader reader = cmd.ExecuteReader();
+            string res="";
+            string name;
+            while (reader.Read())
+            {
+                name = reader.GetString(0) + " " + reader.GetString(1)[0] + ". " + reader.GetString(2)[0] + ".";//Фамилия имя отчество
+                res = res+reader.GetDateTime(3).ToShortDateString()+" " + name + '\n';
+            }
+            if(res == "") { res = "В указанном диапазоне записи не были обнаружены"; }
+            MessageBox.Show(res,listBox_EmployOrUnemploy.Text);
+
+            
+
+            database.closeConnection();
+
         }
     }
 }
